1870daecd605992504c67f60a90c805b5eac2978
Amando Juarez Quintero <94008443+amandojuarezquintero@users.noreply.github.com>
2021-11-11 15:12:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const canvas = document.getElementById('snake')
+const ctx = canvas.getContext('2d')
+
+let speed = 7;
+
+function drawGame(){
+     clearScreen();
+     setTimeout(drawGame, 1000/ speed)
+}
+
+
+function clearScreen(){
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+drawGame();
+
 
