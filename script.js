40ba731171798eb986f649ce28af313d2e4f880b
Amando Juarez Quintero <94008443+amandojuarezquintero@users.noreply.github.com>
2021-11-12 09:58:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ const ctx = canvas.getContext('2d')
 
 let speed = 7;
 
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+let xVelocity=0;
+let yVelocity=0;
+
+//game loop
 function drawGame(){
      clearScreen();
+     drawSnake();
      setTimeout(drawGame, 1000/ speed)
 }
-
 
 function clearScreen(){
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,canvas.width,canvas.height);
 }
+
+function drawSnake(){
+    ctx.fillStile = 'blue'
+    ctx.fillRect(headX * tileCount, headY* tileCount, tileSize,tileSize)
+}
+
+document.body.addEventListener();
+
 
 drawGame();
 
